b22606ab9385f9ae6888a271c71e31acbc25e4c5
Christian Leguizamón <62218305+Emalegui95@users.noreply.github.com>
2020-03-17 19:55:26 -0300

    Inicio de Proyecto
    Primeros Cambios ejercicio V

---
+++ CAI_EjercicioV/CAI_EjercicioV/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+
+namespace CAI_EjercicioV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero, residuo, numero_inv;
+
+            Console.WriteLine("Ingrese un número de 5 cifras: ");
+            numero = int.Parse(Console.ReadLine());
+
+            residuo = numero % 10;
+            numero = numero / 10;
+            numero_inv = residuo * 10;
+
+            residuo = numero % 10;
+            numero = numero / 10;
+            numero_inv = (numero_inv + residuo) * 10;
+
+            residuo = numero % 10;
+            numero = numero / 10;
+            numero_inv = (numero_inv + residuo) * 10;
+
+            residuo = numero % 10;
+            numero = numero / 10;
+            numero_inv = (numero_inv + residuo) * 10;
+
+            numero_inv = numero_inv + numero;
+
+            Console.WriteLine("Su número invertido es: " + numero_inv);
+
+            Console.ReadKey();
+        }
+    }
+}
